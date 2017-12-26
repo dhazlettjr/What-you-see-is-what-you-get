663114be1acf5e6da1f6708dbdbb698b107cd1af
dhazlettjr <d.hazlett.jr@gmail.com>
2017-12-26 15:07:54 -0600

    created people array and DOM elements

---
+++ wysiwyg.js
@@ -0,0 +1,59 @@
+let famousPeeps = [
+    {
+    title: "Guitar legend",
+    name: "Kirk Hammett",
+    bio: "Best guitar player in the world",
+    image: "http://www.mesaboogie.com/media/Artists/Kirk%20Hammett/Kirk-Hammett-400.jpg",
+    lifespan: {
+      birth: 1975,
+      death: 2040
+    }
+  },
+
+   {
+    title: "Mac Master",
+    name: "Steve Jobs",
+    bio: "The Crazy One",
+    image: "https://pbs.twimg.com/profile_images/36850962/steve4_400x400.jpg",
+    lifespan: {
+      birth: 1955,
+      death: 2011
+    }
+   },
+
+   {
+
+    title: "legend",
+    name: "Kirk Hammett",
+    bio: "Best guitar player in the world",
+    image: "http://www.mesaboogie.com/media/Artists/Kirk%20Hammett/Kirk-Hammett-400.jpg",
+    lifespan: {
+    birth: 1975,
+    death: 2040
+    }
+   },
+
+   {
+
+    title: "legend",
+    name: "Kirk Hammett",
+    bio: "Best guitar player in the world",
+    image: "http://www.mesaboogie.com/media/Artists/Kirk%20Hammett/Kirk-Hammett-400.jpg",
+    lifespan: {
+    birth: 1975,
+    death: 2040
+    }
+   },
+
+   {
+
+    title: "legend",
+    name: "Kirk Hammett",
+    bio: "Best guitar player in the world",
+    image: "http://www.mesaboogie.com/media/Artists/Kirk%20Hammett/Kirk-Hammett-400.jpg",
+    lifespan: {
+    birth: 1975,
+    death: 2040
+    }
+   }
+]
