50752fec7e67f8319ed47ec50a8bbd252c49830c
dhazlettjr <d.hazlett.jr@gmail.com>
2017-12-28 18:38:46 -0600

    created object

--- wysiwyg.js
+++ wysiwyg.js
@@ -23,13 +23,13 @@ let famousPeeps = [
 
    {
 
-    title: "legend",
-    name: "Kirk Hammett",
-    bio: "Best guitar player in the world",
-    image: "http://www.mesaboogie.com/media/Artists/Kirk%20Hammett/Kirk-Hammett-400.jpg",
+    title: "Savior",
+    name: "Jesus Christ",
+    bio: "Son of GOD",
+    image: "https://pbs.twimg.com/profile_images/1207702065/jesus2_400x400.jpg",
     lifespan: {
-    birth: 1975,
-    death: 2040
+    birth: 1,
+    death: 4
     }
    },
 
@@ -57,3 +57,18 @@ let famousPeeps = [
     }
    }
 ]
+
+let container = document.getElementById("container");
+let person = document.getElementsByTagName("header")[0];
+
+function personSelect(item){
+   container.style.border = " thick solid #0000FF";
+    
+}
+
+person.addEventListener("click", function(){
+    let clickPerson = event.target;
+    
+    personSelect(clickPerson);
+
+});
